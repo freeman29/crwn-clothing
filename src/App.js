3e2ea66054976bab6ae65bab00c020b19690724a
freeman29 <tordafrancis@gmail.com>
2020-02-16 11:35:28 +0800

    - added react router for routing - updated menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import HomePage from './pages/homepage.component';
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
+
 function App() {
-  return <HomePage />;
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
+    </Switch>
+  );
 }
 
 export default App;
