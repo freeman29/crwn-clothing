90db30653cbb4e90e9d7b3518b14b6626be138a9
freeman29 <tordafrancis@gmail.com>
2020-02-23 20:17:05 +0800

    - added header and link components - add global styles for link tag

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPAge from './pages/shop/shop.component';
 import './App.css';
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPAge} />
-    </Switch>
+    <React.Fragment>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPAge} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
